263e6b61f216b8c5d35ca406dc5f853fa6805982
takkota <kota.taka@onlybiz.net>
2018-12-08 22:59:54 +0900

    populate defalut id

--- src/main/kotlin/net/onlybiz/botchan/database/AppUser.kt
+++ src/main/kotlin/net/onlybiz/botchan/database/AppUser.kt
@@ -8,8 +8,9 @@ import javax.persistence.*
 @Table(name = "app_users")
 data class AppUser(
         @Id
-        @Column(name = "app_user_id")
-        var id: String,
+        @Column(name = "app_user_id", unique = true, nullable = false)
+        // dataクラスで定義する場合、デフォルト値を入れておかないとInstantiationException: No default constructor for entityが発生する
+        var id: String = UUID.randomUUID().toString(),
 
         @Column(name = "line_id")
         var lineId: String? = null,
