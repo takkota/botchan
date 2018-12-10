60ca5491a7dfb0ea6b894a4d8a35ec7778dbc78f
takkota <kota.taka@onlybiz.net>
2018-12-11 01:21:10 +0900

    bug fix

---
+++ src/main/kotlin/net/onlybiz/botchan/database/GroupRepository.kt
@@ -0,0 +1,9 @@
+package net.onlybiz.botchan.database
+
+import net.onlybiz.botchan.database.Test
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface GroupRepository: JpaRepository<Group, String> {
+}
