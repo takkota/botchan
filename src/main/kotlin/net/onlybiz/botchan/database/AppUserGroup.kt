9e3d04d96a46f4d305f012d12b898c15ef5aeaaf
takkota <kota.taka@onlybiz.net>
2018-12-11 20:40:24 +0900

    add id

--- src/main/kotlin/net/onlybiz/botchan/database/AppUserGroup.kt
+++ src/main/kotlin/net/onlybiz/botchan/database/AppUserGroup.kt
@@ -8,28 +8,18 @@ import javax.persistence.*
 @Entity
 @Table(name = "app_user_groups")
 data class AppUserGroup(
-        @AttributeOverrides(
-                AttributeOverride(name = "appUserId",column = Column(name = "app_user_id", length = 36)),
-                AttributeOverride(name = "groupId",column = Column(name = "group_id", length = 36))
-        )
-        @EmbeddedId
-        var appUserGroupId: AppUserGroupId? = null,
-
-        @Column(name="display_name")
-        var displayName: String? = null,
+        @Id
+        @Column
+        @GeneratedValue(strategy= GenerationType.IDENTITY)
+        var id: String? = null,
 
         @ManyToOne
-        @MapsId("appUserId")
         var appUser: AppUser? = null,
 
         @ManyToOne
-        @MapsId("groupId")
-        var group: Group? = null
+        var group: Group? = null,
+
+        @Column(name="display_name")
+        var displayName: String? = null
 
 ) : CommonEntity()
-
-@Embeddable
-class AppUserGroupId(
-        var appUserId: String? = null,
-        var groupId: String? = null
-): Serializable
