cd1ab9c5697298c9c4aeef88382f8cadfdc75d6a
takkota <kota.taka@onlybiz.net>
2018-12-12 22:12:25 +0900

    bug fix

--- src/main/kotlin/net/onlybiz/botchan/controller/api/AccountController.kt
+++ src/main/kotlin/net/onlybiz/botchan/controller/api/AccountController.kt
@@ -3,7 +3,7 @@ package net.onlybiz.botchan.controller.api
 import net.onlybiz.botchan.database.AppUser
 import net.onlybiz.botchan.database.AppUserRepository
 import net.onlybiz.botchan.database.TestRepository
-import net.onlybiz.botchan.model.api.request.LinkTokenParameter
+import net.onlybiz.botchan.model.api.parameter.LinkTokenParameter
 import net.onlybiz.botchan.model.api.response.LinkAppResponse
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
