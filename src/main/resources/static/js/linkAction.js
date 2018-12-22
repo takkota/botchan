3390ccd66c51f702584c518b83e1fa439adee981
takkota <kota.taka@onlybiz.net>
2018-12-23 02:13:39 +0900

    liff

--- src/main/resources/static/js/linkAction.js
+++ src/main/resources/static/js/linkAction.js
@@ -1,11 +1,10 @@
 window.onload = function (e) {
     liff.init(function (data) {
         liff.openWindow({
-            url: "https://botchan.page.link/?" +
-            "link=https://sheltered-scrubland-23764.herokuapp.com/link_start" +
+            url: "https://botchan.page.link/link_start?" +
+            "link=https://sheltered-scrubland-23764.herokuapp.com/link_start?userId=aaaa" +
             "&apn=net.onlybiz.botchanclient" +
-            "&isi=123456789&ibi=net.onlybiz.botchanClient" +
-            "&userId=aaaaa",
+            "&isi=123456789&ibi=net.onlybiz.botchanClient",
             external: true
         });
     });
