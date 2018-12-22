dd9914026126ae64d28b377645c2f137eccdaa99
takkota <kota.taka@onlybiz.net>
2018-12-23 03:04:32 +0900

    test

--- src/main/resources/static/js/linkAction.js
+++ src/main/resources/static/js/linkAction.js
@@ -1,16 +1,10 @@
 window.onload = function (e) {
     liff.init(function (data) {
         liff.openWindow({
-            url: "https//botchan.page.link/link_start" +
-            "?link=https%3A%2F%2Fsheltered-scrubland-23764.herokuapp.com%2Flink_start%3FuserId%3Daaa" +
+            url: "https://botchan.page.link/link_start?" +
+            "link=https://sheltered-scrubland-23764.herokuapp.com/link_start?userId=aaaa" +
             "&apn=net.onlybiz.botchanclient" +
-            "&ibi=net.onlybiz.botchanClient" +
-            "&isi=123456789" +
-            "&userId=bbbb",
-            //url: "https://botchan.page.link/link_start?" +
-            //"link=https://sheltered-scrubland-23764.herokuapp.com/link_start?userId=aaaa" +
-            //"&apn=net.onlybiz.botchanclient" +
-            //"&isi=123456789&ibi=net.onlybiz.botchanClient",
+            "&isi=123456789&ibi=net.onlybiz.botchanClient",
             external: true
         });
     });
