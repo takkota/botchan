50664d3f6a979fb7243ce7dbf43284416b58001e
takkota <kota.taka@onlybiz.net>
2018-12-23 09:26:40 +0900

    test

--- src/main/resources/static/js/linkAction.js
+++ src/main/resources/static/js/linkAction.js
@@ -1,7 +1,10 @@
 window.onload = function (e) {
     liff.init(function (data) {
         liff.openWindow({
-            url: url,
+            url: "https://botchan.page.link/" +
+            "?ibi=net.onlybiz.botchanClient" +
+            "&isi=123456789&apn=net.onlybiz.botchanclient" +
+            "&link=https%3A%2F%2Fsheltered-scrubland-23764.herokuapp.com%3FuserId%3Dabc",
             external: true
         });
     });
