d88ad1ce4b20d58075b5a0b58e10bc9cf496093f
takkota <kota.taka@onlybiz.net>
2019-01-02 02:07:37 +0900

    fix

--- src/main/kotlin/net/onlybiz/botchan/advice/GlobalControllerExceptionHandler.kt
+++ src/main/kotlin/net/onlybiz/botchan/advice/GlobalControllerExceptionHandler.kt
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
+import javax.servlet.http.HttpServletRequest
 
 
 @ControllerAdvice  // Makes this the default behaviour of all controllers
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.ResponseStatus
 class GlobalControllerExceptionHandler {
     @ExceptionHandler(Exception::class)  // Catch any exception
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  // Returns an error code 500
-    fun handleException() {
+    fun handleException(req: HttpServletRequest, exception: Exception) {
+        print("error message" + exception.message)
+        print("error cause" + exception.cause?.message)
     }
 }
