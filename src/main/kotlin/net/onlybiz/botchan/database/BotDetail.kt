ea0ff7bb38274e16fcffa25d4e1df3e5e227b9c7
takkota <kota.taka@onlybiz.net>
2019-01-02 03:06:38 +0900

    fix

--- src/main/kotlin/net/onlybiz/botchan/database/BotDetail.kt
+++ src/main/kotlin/net/onlybiz/botchan/database/BotDetail.kt
@@ -1,24 +1,10 @@
 package net.onlybiz.botchan.database
 
-import com.google.cloud.storage.*
-import com.linecorp.bot.model.message.ImageMessage
 import javax.persistence.*
 import javax.validation.constraints.NotNull
 import com.linecorp.bot.model.message.Message
 import com.linecorp.bot.model.objectmapper.ModelObjectMapper
 import javax.persistence.AttributeConverter
-import com.google.cloud.storage.BlobId
-import org.springframework.cglib.core.CollectionUtils.bucket
-import com.google.auth.oauth2.GoogleCredentials
-import com.google.firebase.FirebaseApp
-import com.google.firebase.FirebaseOptions
-import com.google.firebase.cloud.StorageClient
-import java.io.FileInputStream
-
-
-
-
-
 
 @Entity
 @Table(name = "bot_detail")
@@ -40,7 +26,6 @@ data class BotDetail(
         @OneToOne(fetch = FetchType.LAZY, cascade = [CascadeType.ALL], mappedBy = "botDetail")
         var botPushSchedule: BotPushSchedule? = null,
 
-        @Column(nullable = false)
         @Convert(converter = MessageConverter::class)
         var message: Message? = null
 
@@ -48,12 +33,22 @@ data class BotDetail(
 
 class MessageConverter: AttributeConverter<Message, String> {
         // LineSdkで使用しているObjectMapperを使う
-        override fun convertToDatabaseColumn(message: Message): String {
-                val mapper = ModelObjectMapper.createNewObjectMapper()
-                return mapper.writeValueAsString(message)
+        override fun convertToDatabaseColumn(message: Message?): String? {
+                print("testd:convert")
+                if (message != null) {
+                        print("testd:haveMessage")
+                        val mapper = ModelObjectMapper.createNewObjectMapper()
+                        return mapper.writeValueAsString(message)
+                }
+                return null
         }
-        override fun convertToEntityAttribute(source: String): Message {
-                val mapper = ModelObjectMapper.createNewObjectMapper()
-                return mapper.readValue(source, Message::class.java)
+        override fun convertToEntityAttribute(source: String?): Message? {
+                print("testd:convertFromDB")
+                if (source != null) {
+                        print("testd:noData")
+                        val mapper = ModelObjectMapper.createNewObjectMapper()
+                        return mapper.readValue(source, Message::class.java)
+                }
+                return null
         }
 }
