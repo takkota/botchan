7da905282962ab03a6bc145d2fa86fd9c2656cbf
takkota <kota.taka@onlybiz.net>
2019-01-02 01:50:40 +0900

    fix

---
+++ src/main/kotlin/net/onlybiz/botchan/advice/GlobalControllerExceptionHandler.kt
@@ -0,0 +1,17 @@
+package net.onlybiz.botchan.advice
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+
+
+@ControllerAdvice  // Makes this the default behaviour of all controllers
+@ConditionalOnProperty(prefix = "app", name = arrayOf("disable-default-exception-handling"))
+internal class GlobalControllerExceptionHandler {
+    @ExceptionHandler(Exception::class)  // Catch any exception
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)  // Returns an error code 500
+    fun handleException() {
+    }
+}
