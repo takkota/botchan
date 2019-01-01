3a1dbf349a9130e8f72b35d2bd90b66ed07c0e78
takkota <kota.taka@onlybiz.net>
2019-01-02 03:00:48 +0900

    fix

--- src/main/kotlin/net/onlybiz/botchan/service/BotService.kt
+++ src/main/kotlin/net/onlybiz/botchan/service/BotService.kt
@@ -77,7 +77,8 @@ class BotService {
     fun findBotList(userId: String): List<BotDetail>? {
         return try {
             val user = appUserRepository.findById(userId)
-            return listOf()
+            print("testd:fetch" + user.isPresent)
+            print("testd:lineId" + user.get().lineId)
             return user.get().botDetails
         } catch (e: Exception) {
             print(e.message)
