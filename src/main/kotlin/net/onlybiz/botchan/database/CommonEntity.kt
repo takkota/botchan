6d3b23c6a37b3b5ae77ded1e319380579edba689
takkota <kota.taka@onlybiz.net>
2019-01-03 02:31:07 +0900

    test

--- src/main/kotlin/net/onlybiz/botchan/database/CommonEntity.kt
+++ src/main/kotlin/net/onlybiz/botchan/database/CommonEntity.kt
@@ -6,14 +6,13 @@ import javax.persistence.MappedSuperclass
 import javax.persistence.PreUpdate
 import javax.persistence.PrePersist
 
-
 @MappedSuperclass
 abstract class CommonEntity {
-    @Column(name = "created")
-    private var createdDatetime: Date? = null
+    @Column(name = "created", updatable = false)
+    private var createdDatetime: Date = Date()
 
-    @Column(name = "modified")
-    private var modifiedDateTime: Date? = null
+    @Column(name = "modified", updatable = true)
+    private var modifiedDateTime: Date = Date()
 
     @PrePersist
     fun onPrePersist() {
